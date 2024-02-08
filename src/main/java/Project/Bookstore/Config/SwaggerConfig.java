02771944c1c42135b15ec3d656a148df42a7e5bc
anjdydhody <a01066444534@gmail.com>
2024-02-08 19:47:22 +0900

    feat: add swagger configuration

---
+++ src/main/java/Project/Bookstore/Config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package Project.Bookstore.Config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("bookstore")
+                        .description("책 정보 API"));
+    }
+}
+
--- src/main/java/Project/Bookstore/Controller/BookController.java
+++ src/main/java/Project/Bookstore/Controller/BookController.java
@@ -3,6 +3,9 @@ package Project.Bookstore.Controller;
 import Project.Bookstore.Data.Dto.Req.BookPostReq;
 import Project.Bookstore.Data.Dto.Res.BookGetRes;
 import Project.Bookstore.Service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,22 +14,35 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/books")
+@RequestMapping("/api/books")
 @RequiredArgsConstructor
 public class BookController {
     private final BookService bookService;
 
     @PostMapping
+    @Operation(summary = "책 정보 게시 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공")
+    })
     public ResponseEntity<HttpStatus> post(@RequestBody BookPostReq bookPostReq) {
         return bookService.post(bookPostReq);
     }
 
     @GetMapping
+    @Operation(summary = "책 정보 id로 검색해서 조회 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 id")
+    })
     public ResponseEntity<BookGetRes> get(@RequestParam Integer id) {
         return bookService.get(id);
     }
 
     @GetMapping("/list")
+    @Operation(summary = "책 정보 리스트로 조회 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공")
+    })
     public ResponseEntity<List<BookGetRes>> getList() {
         return bookService.getList();
     }
