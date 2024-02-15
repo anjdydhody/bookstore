24183ea5ba4484a1746905ddd9a9b40f3fc89aaf
anjdydhody <a01066444534@gmail.com>
2024-02-15 16:38:37 +0900

    feat: add cors configuration

--- src/main/java/Project/Bookstore/Config/WebMvcConfig.java
+++ src/main/java/Project/Bookstore/Config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package Project.Bookstore.Config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("file:src/main/resources/static/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
